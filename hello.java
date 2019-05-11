05622dbb44d0bd8f146d62860d38bc3e21fe27c8
jerrygmail <45858805+jerrygmail@users.noreply.github.com>
2019-05-12 03:41:01 +0800

    Edited response

--- hello.java
+++ hello.java
@@ -21,7 +21,7 @@
         static class MyHandler implements HttpHandler {
             @Override
             public void handle(HttpExchange t) throws IOException {
-                String response = "This is the response";
+                String response = "This is the response2";
                 t.sendResponseHeaders(200, response.length());
                 OutputStream os = t.getResponseBody();
                 os.write(response.getBytes());
