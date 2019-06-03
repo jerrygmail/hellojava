f94264a7781449c4d77a3af4878ade928c5404d9
jerrygmail <jerrymanaloto@gmail.com>
2019-06-04 01:55:38 +0800

    Update copyartifact.bat

--- copyartifact.bat
+++ copyartifact.bat
@@ -1,1 +1,2 @@
-curl -uadmin:password -T C:\Jerry\java\hello.class%1% "http://localhost:8081/artifactory/example-repo-local/"
+copy C:\Jerry\java\hello.class C:\Jerry\java\hello.class%1
+curl -uadmin:password -T C:\Jerry\java\hello.class%1 "http://localhost:8081/artifactory/example-repo-local/"
