13e98f1a4037b46bdaf39c2bbe9db2c144558468
jerrygmail <jerrymanaloto@gmail.com>
2023-06-25 00:03:06 +0800

    change to test webhook , 4th time. changed AWS security group to allow all hosts

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
     public static void main(String[] args)
     {
-            System.out.println("hello world hook3");
+            System.out.println("hello world hook4");
     }
 }
